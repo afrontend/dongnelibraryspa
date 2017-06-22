899634a865670834be0fbbacd8d76ff108fdc10e
afrontend <frontend.wordpress@gmail.com>
2017-06-23 00:02:49 +0900

    Update dongnelibrary (v0.1.0)

--- routes/books.js
+++ routes/books.js
@@ -1,47 +1,42 @@
 var express = require('express');
 var router = express.Router();
-var dongnelibrary = require('dongnelibrary');
+var dl = require('dongnelibrary');
 
 /* GET books listing. */
-router.get('/search', function(req, res, next) {
-    var title = 'javascript';
-    var libraryName = '남양도서관';
+router.get('/search', function (req, res, next) {
+  var title = 'javascript';
+  var libraryName = '남양도서관';
 
-    //console.log('query: '+ JSON.stringify(req.query, null, 2));
+  if (req.query.title) {
+    title = req.query.title
+  }
 
-    if(req.query.title) {
-      title = req.query.title
+  if (req.query.libraryName) {
+    libraryName = req.query.libraryName
+  }
+
+  dl.search({
+    title: title,
+    libraryName: libraryName
+  }, function (err, book) {
+    var libraryData = {
+      libraryName: libraryName,
+      books: []
+    };
+    if (!err) {
+      book.booklist.forEach(function (book) {
+        libraryData.books.push({
+          title: book.title,
+          exist: book.exist
+        })
+      });
     }
-
-    if(req.query.libraryName) {
-      libraryName = req.query.libraryName
-    }
-
-    dongnelibrary.search({
-        title: title,
-        libraryName: libraryName
-      }, function (books) {
-
-        var libraryData = {
-          libraryName: libraryName,
-          books: []
-        };
-
-        books.forEach(function (book) {
-            //console.log(book.title);
-            libraryData.books.push({
-              title: book.title,
-              exist: book.exist
-            })
-        });
-
-        res.json(libraryData);
-
-    });
+    res.json(libraryData);
+  });
 });
 
 router.get('/libraryNames', function(req, res, next) {
-    res.json(dongnelibrary.getLibraryNames());
+  res.json(dl.getLibraryNames());
 });
 
 module.exports = router;
